d698eb55fa249145c9aefc40208fbaef58bd0197
zheng.xie <xiezh01@gmail.com>
2017-08-15 23:03:59 +0800

    20170815

--- Algorithm/Array(数组)/485#Max Consecutive Ones.java
+++ Algorithm/Array(数组)/485#Max Consecutive Ones.java
@@ -6,9 +6,9 @@
     
 	解题思路：
     1.便利整个长度的数组
-	2.声明一个变量count用于统计连续出现字符的次数，声明变量result用于比较历史数据和count的值，返回最大的值即可。
-	3.因为只统计连续出现的1。所以我们可以使用Math.max方法。当nums[i]为1的时候，count值加1，表示连续数字当前为1。
-	  将出现1状态的值保存并与result比较。这样便利一遍后，返回的result即为连续出现1的最大值了。
+    2.声明一个变量count用于统计连续出现字符的次数，声明变量result用于比较历史数据和count的值，返回最大的值即可。
+    3.因为只统计连续出现的1。所以我们可以使用Math.max方法。当nums[i]为1的时候，count值加1，表示连续数字当前为1。
+      将出现1状态的值保存并与result比较。这样便利一遍后，返回的result即为连续出现1的最大值了。
 */
 public class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
--- Algorithm/Array(数组)/561#Array Partition I.java
+++ Algorithm/Array(数组)/561#Array Partition I.java
@@ -4,7 +4,7 @@
   问题描述:Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
   解题思路：1.由于题目给出的数量为2n，所以只考虑偶数的情况。即长度为：nums.length
             2.题目要求每一组的数据的最小数相加后，结果尽可能的保持最大。那么其实就是告诉我们要先进行正序排序，这样相加出来的结果才会尽可能的大。
-			3.理清楚思路后，我们实现代码，即先排序，后相加首个数字即可。
+            3.理清楚思路后，我们实现代码，即先排序，后相加首个数字即可。
 */
 public class Solution {
     public int arrayPairSum(int[] nums) {
