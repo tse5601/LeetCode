954e936b38a2fd0205b732e104ab329b51dd838a
zheng.xie <xiezh01@gmail.com>
2017-09-11 22:22:28 +0800

    杨辉三角

---
+++ Algorithm/Array(数组)/119#Pascal's Triangle.java
@@ -0,0 +1,22 @@
+/*
+	ID：119
+	标题：Pascal's Triangle II
+	问题描述：
+    杨辉三角,只输出一行
+    
+*/
+class Solution {
+      public List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new ArrayList<Integer>();
+        if (rowIndex < 0)
+            return list;
+
+        for (int i = 0; i < rowIndex + 1; i++) {
+            list.add(0, 1);
+            for (int j = 1; j < list.size() - 1; j++) {
+                list.set(j, list.get(j) + list.get(j + 1));
+            }
+        }
+        return list;
+    }
+}
